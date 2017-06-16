7a2adf5d3f547b4595da953f5b89ede1de37430b
Lino <linojr@gmail.com>
2017-06-16 18:31:36 -0500

    rearranged buttons

--- resources/main.js
+++ resources/main.js
@@ -11,37 +11,56 @@ function clearEntry() {
 //function to clear all
 function clearAll(){
   clearEntry();
-  document.getElementById("value").textContent = "0";
   previousValue = 0;
   currentValue = 0;
+  operandPressed = "";
+  result = 0;
+  hasDoneMath = false;
+  hasPrevValue = false;
+  document.getElementById("value").textContent = currentValue;
 }
-
 //when I press a button, then I see that value show up in the display.
 function buttonPress(value){
+  if (hasDoneMath){
+    clearEntry();
+    document.getElementById("value").textContent = numberPressed;
+    currentValue = getCurrentValue();
+    hasDoneMath = false;
+  }
   var numberPressed = value.textContent;
   currentValue = getCurrentValue();
   if (currentValue == "0") {
-    // var addToDisplay =  document.getElementById(numberPressed).textContent;
     document.getElementById("value").textContent = numberPressed;
     currentValue = getCurrentValue();
-  } else if (currentValue != "0") {
+  } else {
     document.getElementById("value").textContent = currentValue + numberPressed;
     currentValue = getCurrentValue();
   }
+  console.log(currentValue);
+
   return currentValue;
 }
+var hasCurrValue = true;
+var hasPrevValue = false;
+var currentValue = 0;
+var previousValue = 0;
+var operandPressed = "";
+var result = 0;
 
-var currentValue;
-var previousValue;
-var operandPressed;
 // when I click on an operator, then the input goes out
-function applyOperator(operand){
-  previousValue = currentValue;
+// when I click on an operator, and I've inputed values before, then I can keep chaining operations.
+function applyOperator(operand) {
+  if (hasCurrValue && hasPrevValue) {
+    doMath(currentValue, previousValue, operandPressed);
+  } else {
   operandPressed = operand.textContent;
-  console.log(operandPressed);
+  hasPrevValue = true;
+  previousValue = currentValue;
   clearEntry();
+  }
 }
 
+// dictionary to do math given multiple inputs.
 var doOperations = {
   "+" : function(x,y){return x + y},
   "-" : function(x,y){return x - y},
@@ -49,15 +68,18 @@ var doOperations = {
   "/" : function(x,y){return x / y}
 }
 
+
+var hasDoneMath = false;
 function doMath(value1, value2, operand){
-  //if response longer than 9 places, then display "digit limit reached"
   if(false){
-
+    //if response longer than 9 places, then display "digit limit reached"
   } else if (true){
-  //if less than 9 places, then calculate.
-    var x = parseInt(value1);
-    var y = parseInt(value2);
-    console.log(doOperations[operand](x,y));
+    //if less than 9 places, then calculate.
+    var x = parseFloat(value1);
+    var y = parseFloat(value2);
+    result = doOperations[operand](x,y);
+    currentValue = result;
+    document.getElementById("value").textContent = currentValue;
+    hasDoneMath = true;
   }
-
 }
